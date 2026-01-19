f7f583fcdbd289be43a2ba7a635fee83177a88c6
Ian Choo <ianchootz2004@gmail.com>
2026-01-19 16:37:50 +0800

    Level 0.1
    - Corrected the greeting statement.

--- src/main/java/JMe.java
+++ src/main/java/JMe.java
@@ -6,7 +6,7 @@ public class JMe {
                 + " | |_| | |  | |  __/\n"
                 + "  \\___/|_|  |_|\\___|\n";
         String horizontalLine = "____________________________________________________________\n";
-        System.out.println(horizontalLine + "Hello from JMe\n" + logo + "What can I do for you?");
+        System.out.println(horizontalLine + "Hello! I'm JMe!\n" + logo + "What can I do for you?");
         System.out.println(horizontalLine + "Bye. Hope to see you again soon!\n" + horizontalLine);
     }
 }
