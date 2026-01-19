ca46fa49ac9b219510e8bc678a5d70a49bc31cd7
Ian Choo <ianchootz2004@gmail.com>
2026-01-19 16:36:06 +0800

    Level 0
    - Rename to JMe
    - Implemented an initial skeletal version of JMe that simply greets the user and exits.

---
+++ src/main/java/JMe.java
@@ -0,0 +1,12 @@
+public class JMe {
+    public static void main(String[] args) {
+        String logo = "      _ __  __\n"
+                + "     | |  \\/  | ___\n"
+                + "  _  | | |\\/| |/ _ \\\n"
+                + " | |_| | |  | |  __/\n"
+                + "  \\___/|_|  |_|\\___|\n";
+        String horizontalLine = "____________________________________________________________\n";
+        System.out.println(horizontalLine + "Hello from JMe\n" + logo + "What can I do for you?");
+        System.out.println(horizontalLine + "Bye. Hope to see you again soon!\n" + horizontalLine);
+    }
+}
