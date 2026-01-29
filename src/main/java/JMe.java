a1198da971f7aed812afef192119e8897fb7d211
Ian Choo <ianchootz2004@gmail.com>
2026-01-29 19:54:48 +0800

    Level 1
    Added the ability for JMe to echo.

--- src/main/java/JMe.java
+++ src/main/java/JMe.java
@@ -1,12 +1,40 @@
+import java.util.Scanner;
+
 public class JMe {
-    public static void main(String[] args) {
+
+    //constants or formats
+    public static final String horizontalLine = "____________________________________________________________";
+
+    public static void greetUser() {
         String logo = "      _ __  __\n"
                 + "     | |  \\/  | ___\n"
                 + "  _  | | |\\/| |/ _ \\\n"
                 + " | |_| | |  | |  __/\n"
                 + "  \\___/|_|  |_|\\___|\n";
-        String horizontalLine = "____________________________________________________________\n";
-        System.out.println(horizontalLine + "Hello! I'm JMe!\n" + logo + "What can I do for you?");
-        System.out.println(horizontalLine + "Bye. Hope to see you again soon!\n" + horizontalLine);
+
+        System.out.println(horizontalLine + "\nHello! I'm JMe!\n" + logo + "What can I do for you?\n" + horizontalLine);
+    }
+
+    public static void byeUser() {
+        System.out.println(horizontalLine + "\nBye. Hope to see you again soon!\n" + horizontalLine);
+    }
+
+    public static void readUserInput() {
+        String userInput;
+        Scanner in = new Scanner(System.in);
+
+        userInput = in.nextLine();
+        userInput.replace("\n", "");
+
+        while(!(userInput.toLowerCase().equals("bye")) ) {
+            System.out.println(horizontalLine + "\n" + userInput + "\n" + horizontalLine);
+            userInput = in.nextLine();
+        }
+        byeUser();
+    }
+
+    public static void main(String[] args) {
+        greetUser();
+        readUserInput();
     }
 }
